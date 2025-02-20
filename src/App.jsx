42d3dbffa2ae83177da5bb30bc4779d467f7c548
Tyrell <tyrellkamara-oliver@pursuit.org>
2025-02-20 10:46:55 -0500

    changed home.js to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
+import Home from "./Pages/Home";
+
 function App() {
   return (
     <div className="app">
